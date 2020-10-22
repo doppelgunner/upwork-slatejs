f037dd8c4b197c83092d17bf2a7850032e72ca35
doppelgunner <doppelgunner@gmail.com>
2020-10-22 14:43:13 +0800

    Updates

---
+++ precache-manifest.2bec1b5d658b8cf653c7122f8eec0495.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcb285f93d1cb6b5879a4a45f2c18b1a",
+    "url": "/upwork-slatejs/index.html"
+  },
+  {
+    "revision": "d51f43fdc57feafd9a4b",
+    "url": "/upwork-slatejs/static/css/main.7f9caff9.chunk.css"
+  },
+  {
+    "revision": "7921ec72ccbb6dc1902c",
+    "url": "/upwork-slatejs/static/js/2.bb2eac6c.chunk.js"
+  },
+  {
+    "revision": "f9e85ffb8fb4043b5cf3fe48c5e4a3e9",
+    "url": "/upwork-slatejs/static/js/2.bb2eac6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d51f43fdc57feafd9a4b",
+    "url": "/upwork-slatejs/static/js/main.3c7dea91.chunk.js"
+  },
+  {
+    "revision": "1e02dc89464c0467a684",
+    "url": "/upwork-slatejs/static/js/runtime-main.7aaa3ec7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/upwork-slatejs/static/media/logo.5d5d9eef.svg"
+  }
+]);
